777407ae378d5f49ae2dcb5d88a4474727f3dc68
Hannah Catherine Trask <hctrask@gmail.com>
2020-09-16 08:46:16 -0600

    further update

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
 /****JS*****/
-/////navigation bar on click
+// CHECKING FOR JQUERY LINK
 console.log('jquery linked');
 
-// let sheetUrl =
-// 	'https://docs.google.com/spreadsheets/d/1ZLt9vCIk7jeJkFNivzKThYBwhRwlu3JCcRIsmAKSKzk/edit?usp=sharing';
+// // GOOGLE SHEET URL FOR AJAX CALL
+// // let sheetUrl =
+// // 	'https://docs.google.com/spreadsheets/d/1ZLt9vCIk7jeJkFNivzKThYBwhRwlu3JCcRIsmAKSKzk/edit?usp=sharing';
 
-//AJAX CALL
 
+
+//AJAX CALL FROM GOOGLE SHEET
+//json link variable, this is what we are calling
 let sheetAsJSON =
 	'https://spreadsheets.google.com/feeds/list/1ZLt9vCIk7jeJkFNivzKThYBwhRwlu3JCcRIsmAKSKzk/od6/public/values?alt=json';
 
+//this call will grab the info from the google sheet
 $.ajax({ url: sheetAsJSON }).then((data) => {
-	//return a new array of data with .map()
-	//      plural
+	//this logs to check I've called the data
 	console.log(data.feed.entry);
 
-	const $project = $('.carousel-inner');
+    //this variable grabs all carousel-inner classes
+    const $project = $('.carousel-inner');
+    
+    //this loops through the provided array in data
 	const projects = data.feed.entry.forEach((project) => {
-		const $newProjectDiv = $('<div>').addClass('carousel-item').addClass('active'); //<-----figure out how to make this dynamic
+
+        //this adds a new div and gives it the class carousel-item
+        const $newProjectDiv = $('<div>').addClass('carousel-item').addClass('active'); //<-----figure out how to make this dynamic
+        
+        //this adds a new image and sources it from the array, gives it classes d-block and w-100
 		const $image = $('<img>').attr('src', project.gsx$image.$t).addClass('d-block').addClass('w-100');
         // $newProjectDiv.text(project.gsx$description.$t).addClass('text');
-		$project.append($newProjectDiv);
+
+        //this appends the new div to the carousel
+        $project.append($newProjectDiv);
+        //this appends the image to the newly created div
 		$newProjectDiv.append($image);
     });
     
 
 });
+
+
+// CONTACT FORM FUNCTIONALITY
